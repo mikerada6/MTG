f49f6328655c2ae8a7386ebaaf55efbf1b112e6b
Michael Radaszkiewicz <michael_radaszkiewicz@comcast.com>
2020-09-08 10:16:57 -0400

    initial commit

---
+++ src/main/java/rez/mtg/price/PriceApplication.java
@@ -0,0 +1,13 @@
+package rez.mtg.price;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/rez/mtg/price/ServletInitializer.java
@@ -0,0 +1,13 @@
+package rez.mtg.price;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PriceApplication.class);
+	}
+
+}
